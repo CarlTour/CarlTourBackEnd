83e93525210060b2ecab30a71e6fb52d48b2431c
Nick Jones <nrjones8@gmail.com>
2014-05-05 13:13:26 -0500

    Added mongo and set up an upcoming events API endpoint

--- CarlTour/carltour/__init__.py
+++ CarlTour/carltour/__init__.py
@@ -1,12 +1,43 @@
+from pymongo import MongoClient
+from urllib.parse import urlparse
+from gridfs import GridFS
+
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+    """ 
+    This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+
+    # (route_name, URL)
+    config.add_route('upcoming_events', 'api/v1.0/events')
     config.scan()
+
+    db_url = urlparse(settings['mongo_uri'])
+
+    # The registry "maps resource types to views, as well as housing 
+    # other application-specific component registrations"
+    config.registry.db = MongoClient(
+            host=db_url.hostname
+    )
+
+    # TODO figure out what these do. Taken from Pyramid/mongo tutorial here:
+    # http://pyramid-cookbook.readthedocs.org/en/latest/database/mongodb.html
+    def add_db(request):
+      db = config.registry.db[db_url.path[1:]]
+      if db_url.username and db_url.password:
+        print('authenitacintg?')
+        db.authenticate(db_url.username, db_url.password)
+      return db
+
+    def add_fs(request):
+      print('adding_fs??')
+      return GridFS(request.db)
+
+    config.add_request_method(add_db, 'db', reify=True)
+    config.add_request_method(add_fs, 'fs', reify=True)
+
     return config.make_wsgi_app()
--- CarlTour/carltour/views.py
+++ CarlTour/carltour/views.py
@@ -1,6 +1,16 @@
+from pyramid.view import view_defaults
 from pyramid.view import view_config
 
+@view_defaults(route_name='upcoming_events')
+class UpcomingEventsAPI(object):
+    def __init__(self, request):
+        self.request = request
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'CarlTour'}
+    @view_config(request_method='GET', renderer='json')
+    def get(self):
+        event_collection = self.request.db['events']
+        # Exclude the _id field, but return everything else
+        all_events = list(event_collection.find(fields={'_id' : False}))
+
+        event_dict = {'events' : all_events}
+        return event_dict
